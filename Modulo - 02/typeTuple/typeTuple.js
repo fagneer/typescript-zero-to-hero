6bf660d3fc7c5e8ae1a27296708ac3dcf46c4f28
Fágner Gomes <josefagner6@gmail.com>
2023-07-31 00:33:54 -0300

    Fechado na aula 18.1. Continuarei amanhã 12h.

--- Modulo - 02/typeTuple/typeTuple.js
+++ Modulo - 02/typeTuple/typeTuple.js
@@ -1,1 +1,9 @@
 "use strict";
+let pessoa = ['Fagner Gomes', 'Analista programador', 28];
+console.log(pessoa);
+console.log(pessoa[1]);
+let listaFrutas = [1, 'mamao', 'maça', 'manga', 'laranja'];
+console.log(listaFrutas);
+let pessoa2 = ['Fagner Gomes', 'Analista programador', 28];
+console.log(pessoa2);
+console.log(pessoa2[1]);
---
+++ Modulo - 02/typeTuple/typeTuple.ts
@@ -0,0 +1,10 @@
+let pessoa: [string, string, number] = ['Fagner Gomes', 'Analista programador', 28];
+console.log(pessoa);
+console.log(pessoa[1]);
+
+let listaFrutas: [number, ...string[]] = [1, 'mamao', 'maça', 'manga', 'laranja'];
+console.log(listaFrutas);
+
+let pessoa2: [nome: string, cargo: string, idade: number] = ['Fagner Gomes', 'Analista programador', 28];
+console.log(pessoa2);
+console.log(pessoa2[1]);
